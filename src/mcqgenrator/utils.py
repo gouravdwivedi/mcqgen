7de2d209f9cc5bc7b6cab6371c77bc5f33931312
gouravdwivedi <gourav.dwivedi@gmail.com>
2024-02-26 17:54:20 +0100

    fix pdf read issue

--- src/mcqgenrator/utils.py
+++ src/mcqgenrator/utils.py
@@ -6,7 +6,7 @@ import PyPDF2
 def read_file(file):
     if file.name.endswith(".pdf"):
         try:
-            pdf_reader=PyPDF2.PdfFileReader(file)
+            pdf_reader=PyPDF2.PdfReader(file)
             text=""
             for page in pdf_reader.pages:
                 text+=page.extract_text()
@@ -23,6 +23,10 @@ def read_file(file):
     
 def get_table_data(quiz_str):
     try:
+
+        # data cleaning for pdf
+        quiz_str = remove_text_before_first_occurrence(quiz_str, "{")
+
         # convert the quiz from a str to dict
         quiz_dict=json.loads(quiz_str)
         quiz_table_data=[]
@@ -45,3 +49,10 @@ def get_table_data(quiz_str):
     except Exception as e:
         traceback.print_exception(type(e),e,e.__traceback__)
         return False
+    
+def remove_text_before_first_occurrence(text, delimiter):
+    index = text.find(delimiter)
+    if index != -1:
+        return text[index:]
+    else:
+        return text
